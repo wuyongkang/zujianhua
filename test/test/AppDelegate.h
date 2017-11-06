6972fc3694bd5402f41003c7600ce179ffa6f222
wuyongkang <747442465@qq.com>
2017-11-06 15:07:33 +0800

    你妹

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 邬永康 on 2017/11/6.
+//  Copyright © 2017年 yxlady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 邬永康 on 2017/11/6.
+//  Copyright © 2017年 yxlady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
